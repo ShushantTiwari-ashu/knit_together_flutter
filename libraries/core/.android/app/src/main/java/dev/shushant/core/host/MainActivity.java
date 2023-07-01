296fbd990136562606afc0654dd2ed15a2a3e1d5
shushanttiwari <shushanttiwari.ashu@gmail.com>
2023-07-01 14:01:00 +0530

    initial commit

--- libraries/core/.android/app/src/main/java/dev/shushant/core/host/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package dev.shushant.core.host;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- libraries/data/.android/app/src/main/java/dev/shushant/data/host/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package dev.shushant.data.host;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
